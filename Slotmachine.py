5c4046bb57a5006a11a2352f08727d668159816a
AryanMadan3107 <aryanmadan@outlook.com>
2026-05-31 09:53:27 +0200

    Add files via upload

--- Slotmachine.py
+++ Slotmachine.py
@@ -3,9 +3,52 @@ import random
 from tkinter import messagebox
 
 def rules():
+    global insertmoney
     insertmoney=startingmoney.get()
     startingmoneybutton.config(state="disabled")
-    messagebox.showinfo("Rules","You are going to be gambling with €" + insertmoney + "\n After confirming starting money there is no way back \n 2 same numbers = money left * 1.5 \n 3 same numbers = money left * 3")
+    messagebox.showinfo("Rules","You are going to be gambling with €" + insertmoney + "\n After confirming starting money there is no way back \n 2 same numbers = money left * 1.5 \n 3 same numbers = money left * 3 \n 0 same numbers = money left / 2")
+    balancelbl.config(text="Balance: €"+insertmoney)
+
+def slots():
+    global insertmoney
+    s1=random.randint(0,9)
+    s2=random.randint(0,9)
+    s3=random.randint(0,9)
+
+    slot1.config(text=str(s1))
+    slot2.config(text=str(s2))
+    slot3.config(text=str(s3))
+
+    if s1==s2==s3:
+        insertmoney=3*int(insertmoney)
+        messagebox.showinfo("Result","You won 3x your money!")
+        balancelbl.config(text="Balance: €"+str(insertmoney))
+    
+    elif s1==s2 or s1==s3 or s2==s3:
+        insertmoney=1.5*int(insertmoney)
+        messagebox.showinfo("Result","You won 1.5x your money!")
+        balancelbl.config(text="Balance: €"+str(insertmoney))
+
+
+    else:
+        insertmoney=int(insertmoney)/2
+        messagebox.showinfo("Result","You lost half your money :(")
+        balancelbl.config(text="Balance: €"+str(insertmoney))
+
+def ending():
+    global insertmoney
+    if int(insertmoney)<1:
+        slotbutton.config(state="disabled")
+        messagebox.showinfo("Lost","You lost, find money and try again :(")
+    elif int(insertmoney)>1000:
+        slotbutton.config(state="disabled")
+        messagebox.showinfo("Win","You won, go spend your money")
+    else:
+        slots()
+
+
+    
+
 
 root = Tk()
 root.config(background="#E1B30C")
@@ -27,8 +70,8 @@ startingmoney.grid(row=2,column=0)
 startingmoneybutton=Button(root,text="Click to confirm",command=rules)
 startingmoneybutton.grid(row=3,column=0,pady=10)
 
-balance=Label(frame,text="Balance: €",font=("Arial",10),bg="#E1B30C",fg="white")
-balance.grid(row=1,column=0)
+balancelbl=Label(frame,text="Balance: €",font=("Arial",30),bg="#E1B30C",fg="white")
+balancelbl.grid(row=1,column=0)
 
 slotframe=Frame(root,bg="#E1B30C")
 slotframe.grid(row=4,column=0,pady=100)
@@ -41,6 +84,9 @@ slot2.grid(row=0,column=1)
 
 slot3=Label(slotframe,text="7",font=("Arial",60),bg="#E6D38E",fg="white")
 slot3.grid(row=0,column=2)
+
+slotbutton=Button(root,text="Spin",font=("Arial",20),command=ending)
+slotbutton.grid(row=5,column=0)
 
 mainloop()
 
